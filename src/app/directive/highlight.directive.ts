ce4bf08ce0b11aad23369f6fc55828120531d23e
vanta blanta <vantablanta@gmail.com>
2022-04-11 09:54:10 +0300

    highlight color chnage

--- src/app/directive/highlight.directive.ts
+++ src/app/directive/highlight.directive.ts
@@ -6,7 +6,7 @@ import { Directive, ElementRef, HostListener } from '@angular/core';
 export class HighlightDirective {
 
   constructor(private el: ElementRef) {
-    this.el.nativeElement.style.color = "blue"
+    this.el.nativeElement.style.color = "#0275d8"
   }
 
 
